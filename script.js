dea7e1c09105e8c7f0d31c934db75b7f651c8eb4
Flávio Félix <fau_felix30@hotmail.com>
2022-12-14 19:25:38 -0300

    Feito o requisito 4

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ colors[0].style.backgroundColor = 'rgb(0, 0, 0)';
 colors[1].style.backgroundColor = 'green';
 colors[2].style.backgroundColor = 'orange';
 colors[3].style.backgroundColor = 'blue';
+const generateColors = () => {
+  const corA = Math.floor(Math.random() * 256);
+  const corB = Math.floor(Math.random() * 256);
+  const corC = Math.floor(Math.random() * 256);
+  return `rgb(${corA}, ${corB}, ${corC})`;
+};
+function colorir() {
+  colors[1].style.backgroundColor = generateColors();
+  colors[2].style.backgroundColor = generateColors();
+  colors[3].style.backgroundColor = generateColors();
+}
+const buttao = document.getElementById('button-random-color');
+buttao.addEventListener('click', colorir);
